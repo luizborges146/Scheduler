9be7b18cea1504360731df9a674d8af64474aa01
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-05 16:26:24 -0700

    Added houra nd time, also start adding style to the columns

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var date = moment().format("MM/DD/YYYY");
+// var time = moment().format("HH:mm:ss")
+// var displayTime =  document.getElementById("timer");
+
+
+var updateTime = function () {
+    var currentDate = moment().format("MMMM/DD/YYYY");
+    var currentTime = moment().format(':mm:ss');
+    var currentHour = moment().format('HH');
+    $("#timer").text(currentDate + "  " + currentHour + currentTime)
+}
+updateTime()
+setInterval(updateTime, 1000);
